a0077bdae6e65c0397a913912374159d39499934
Anusha Konatala <anu.konatala@gmail.com>
2018-05-25 19:10:08 +0000

    automatically backed up by learn

---
+++ lib/person.rb
@@ -0,0 +1,16 @@
+class Person
+  
+  def name=(name)
+    @name = name
+  end
+  def name
+    @name  
+  end
+  def breed
+    @breed
+  end
+  
+  def breed=(breed)
+    @breed = breed
+  end
+end
